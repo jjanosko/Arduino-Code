e06e2fa8febb31b0e4a7e0cc3a67950ede99a092
Jacob Janosko <jjanosko@stevens.edu>
2020-02-21 15:21:10 -0500

    adding hw4

---
+++ hw4.s
@@ -0,0 +1,59 @@
+	.file	"hw4.cc"
+	.text
+	.p2align 4
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB2084:
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	jmp	_ZNSt8ios_base4InitD1Ev
+	.seh_endproc
+	.p2align 4
+	.globl	_Z3addii
+	.def	_Z3addii;	.scl	2;	.type	32;	.endef
+	.seh_proc	_Z3addii
+_Z3addii:
+.LFB1602:
+	.seh_endprologue
+	leal	(%rcx,%rdx), %eax
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section	.text.startup,"x"
+	.p2align 4
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1603:
+	subq	$40, %rsp
+	.seh_stackalloc	40
+	.seh_endprologue
+	call	__main
+	xorl	%eax, %eax
+	addq	$40, %rsp
+	ret
+	.seh_endproc
+	.p2align 4
+	.def	_GLOBAL__sub_I__Z3addii;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I__Z3addii
+_GLOBAL__sub_I__Z3addii:
+.LFB2085:
+	subq	$40, %rsp
+	.seh_stackalloc	40
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rcx
+	addq	$40, %rsp
+	jmp	atexit
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z3addii
+.lcomm _ZStL8__ioinit,1,1
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 9.2.0"
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
